d1e6405dd8dcf44e8fa56cb67115d87c5162842c
Yazz <itsjustmaitai@gmail.com>
2025-06-06 16:31:30 +0300

    Created article class

---
+++ lib/modules/article.py
@@ -0,0 +1,10 @@
+class Article:
+    def __init__(self,author,magazine,title):
+        self.author=author
+        self.magazine=magazine
+        self.title=title
+        if not isinstance(title,str):
+            raise ValueError('It has be a string')
+        if not isinstance(5 <= len(title) <=  50):
+            raise TypeError('Has to be between 5 and 50 characters')
+        
