17f6d4f64cd88b468fb6ac68a53f24bf9a9433f4
Yazz <itsjustmaitai@gmail.com>
2025-06-06 15:56:07 +0300

    Added magazine class and changed some names

---
+++ lib/modules/Magazine.py
@@ -0,0 +1,24 @@
+class Magazine:
+    def __init__(self,name,category):
+        self.name=name
+        self.category=category
+        self._articles=[]
+        if not isinstance(2 <= len(name) <= 16):
+            raise TypeError('Name has to be between 2 and 16')
+        if not isinstance(name,str) or name.strip():
+            raise ValueError('Name has to be a string')
+        if not isinstance(category,str) or name.strip():
+            raise ValueError('Name has to be a string')
+        
+        @property
+        def articles(self):
+            return self._articles
+        
+        def contributors(self):
+            return list({magazine.contribitors for magazine in Magazine})
+        
+        def article_title(self):
+            return list({articles.title for article in self._articles})
+        
+        def contributing_authors(self):
+            return list({articles.author for articles in self._articles})
---
+++ lib/modules/author.py
@@ -0,0 +1,30 @@
+class Author:
+    def __init__(self,name):
+        if not isinstance(name,str) or not name.strip():
+            raise ValueError('')
+        self._name=name
+        self._articles=[]
+        
+    @property
+    def name(self):
+        return self._name
+    
+    def articles(self):
+        return self._articles
+    
+    def magazine(self):
+        return list({article.magazine for article in self._articles})
+    
+
+    def add_article(self,magazine,title):
+        from lib.modules.author import Articles
+        article=Articles(self,magazine,title)
+        self._articles.append(article)
+        magazine._article.append(article)
+        return(article)
+    
+    def topic(self):
+        return list({magazine.category for magazine in self.magazine})
+
+    
+        
